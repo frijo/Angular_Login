ba920ad73d5ab5b6c35fdc98786016186c11360b
frijo <frijo45@gmail.com>
2017-08-29 16:32:27 -0600

    Angular Login for RoR API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MaterializeModule } from 'angular2-materialize';
+//import { HomeComponent } from './home/home.component';
+import { Angular2TokenService } from 'angular2-token';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthDialogComponent,
+    ToolbarComponent,
+    LoginFormComponent,
+    RegisterFormComponent
+
   ],
   imports: [
+    FormsModule,
+    HttpModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule,
   ],
-  providers: [],
+  providers: [Angular2TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
